83591ffb308e1b48a41a361c68ad24398ec8aa9b
mohammad <mohammad.hashemi747@gmail.com>
2019-04-20 10:35:12 +0430

    Insert method();
    This method takes the data which is a int data of the next node
    and put it in the head node at the begining but if the head node
    is't empty which means we have more nodes already, the method travers
    to the last node and add the data to it.

--- src/LinkeList.java
+++ src/LinkeList.java
@@ -4,5 +4,17 @@ public class LinkeList {
     public void insert(int data){
         Node node = new Node();
         node.data = data;
+        node.next = null;
+
+        if (head==null){
+            head = node;
+        }
+        else {
+            Node n = head;
+            while (n.next!=null){
+                n = n.next;
+            }
+            n.next = node;
+        }
     }
 }
