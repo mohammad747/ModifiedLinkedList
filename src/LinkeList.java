05b43ca9ff12f9eedb55279930d1bd5f9c29532c
mohammad <mohammad.hashemi747@gmail.com>
2019-04-20 10:43:00 +0430

    Show() method The show method traverses the hole nodes to the null and print all their data on the screen.

--- src/LinkeList.java
+++ src/LinkeList.java
@@ -11,10 +11,23 @@ public class LinkeList {
         }
         else {
             Node n = head;
+
             while (n.next!=null){
                 n = n.next;
             }
             n.next = node;
         }
     }
+
+    public void show(){
+        Node node = head;
+
+        while (node.next!=null){
+
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    
 }
