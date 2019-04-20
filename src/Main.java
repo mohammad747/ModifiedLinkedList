75821ce619e6085ec84dab7acc5690e9b51bb180
mohammad <mohammad.hashemi747@gmail.com>
2019-04-20 10:57:00 +0430

    Manin class
    create the main class to test the insert() and the show() methods.
    but there is a bug in showing the last node data. So I'm going to
    fix this bug in the new branch.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkeList list = new LinkeList();
+
+        list.insert(18);
+        list.insert(45);
+        list.insert(12);
+
+        list.show();
+    }
+}
